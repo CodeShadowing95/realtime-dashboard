f0a9c26117c058e0ba60778d9ce2ea2258badab9
VastoLorde <ksenbonsakura@gmail.com>
2024-02-15 17:34:08 +0100

    Next commit: Workflow setup

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 // import { createClient } from "graphql-ws";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, ForgotPassword, Login, Register } from "./pages";
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
