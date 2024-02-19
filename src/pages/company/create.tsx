8b00a9508e3eae912ca8d8d3232aa045f3cf0fd5
CodeShadowing95 <ksenbonsakura@gmail.com>
2024-02-20 00:17:18 +0100

    Create in progress...

--- src/pages/company/create.tsx
+++ src/pages/company/create.tsx
@@ -1,5 +1,35 @@
+import { CREATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { useModalForm } from "@refinedev/antd";
+import { useGo } from "@refinedev/core"
+import { CompanyList } from "./list";
+import { Modal } from "antd";
+
 export const Create = () => {
-  return (
-    <div>Create new company</div>
-  )
+    const go = useGo();
+
+    const goToListPage = () => {
+        go({
+            to: { resource: 'companies', action: 'list' },
+            options: { keepQuery: true },
+            type: 'replace'
+        })
+    }
+
+    const { formProps, modalProps } = useModalForm({
+        action: 'create',
+        defaultVisible: true,
+        resource: 'companies',
+        redirect: false,
+        mutationMode: 'pessimistic',
+        onMutationSuccess: goToListPage,
+        meta: {
+            gqlMutation: CREATE_COMPANY_MUTATION
+        }
+    })
+
+    return (
+        <CompanyList>
+            <Modal></Modal>
+        </CompanyList>
+    )
 }
